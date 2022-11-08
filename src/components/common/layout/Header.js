b8cb57d20011ab22bda54ae538c3ea8ebc958c79
ilia faramarzpour <44089718+iliafaramarzpour@users.noreply.github.com>
2022-11-08 12:46:44 +0330

    feat : add flowbite tailwind component lib

--- src/components/common/layout/Header.js
+++ src/components/common/layout/Header.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Dropdown } from "flowbite-react";
 
 function Header() {
   return (
     <>
-      <header className="bg-white p-5">header</header>
+      <header className="bg-white p-5 flex justify-start flex-row-reverse">
+        <Dropdown label="Dropdown">
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block text-sm font-medium truncate">
+              bonnie@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        -
+      </header>
     </>
   );
 }
